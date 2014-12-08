7c46b8cea2866db5a98b28f5d87d5db546ef366c
Marko Tunjic <mtunjic@razor.rs>
2014-12-08 16:37:52 +0100

    Clear completed

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -17,9 +17,16 @@ var TodosController = Ember.ArrayController.extend({
 
 			// save the new model
 			todo.save();
-		}
+		},
+		clearCompleted: function() {
+        	var completed = this.filterBy('isCompleted', true);
+        	completed.invoke('deleteRecord');
+        	completed.invoke('save');
+    	}
 	}, // end actions
-	
+
+
+	// this is a computed properties
 	remaining: function() {
 		return this.filterBy('isCompleted', false).get('length');
 	}.property('@each.isCompleted'),
@@ -27,7 +34,15 @@ var TodosController = Ember.ArrayController.extend({
 	inflection: function() {
 		var remaining = this.get('remaining');
 		return (remaining === 1) ? 'item' : 'items';
-	}.property('remaining') 
+	}.property('remaining'),
+
+	hasCompleted: function() {
+    	return this.get('completed') > 0;
+	}.property('completed'),
+
+	completed: function() {
+	    return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted')
 
 });
 
